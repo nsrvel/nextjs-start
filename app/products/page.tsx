b0104017a2947f045196b7eb6fb0503854586368
Putra <putra.rama@suretybond.co.id>
2023-07-07 17:26:13 +0700

    initial commit

---
+++ app/products/page.tsx
@@ -0,0 +1,44 @@
+import AddProduct from './addProduct';
+
+type Product = {
+    id: number;
+    title: string;
+    price: number;
+};
+
+async function getProducts() {
+    const res = await fetch('http://localhost:5000/products', { next: { revalidate: 10 } });
+    return res.json();
+}
+
+export default async function ProductList() {
+    const products: Product[] = await getProducts();
+
+    return (
+        <div className="py-10 px-10">
+            <div className="py-2">
+                <AddProduct />
+            </div>
+            <table className="table w-full">
+                <thead>
+                    <tr>
+                        <th>#</th>
+                        <th>Product Name</th>
+                        <th>Price</th>
+                        <th>Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {products.map((product, index) => (
+                        <tr key={product.id}>
+                            <td>{index + 1}</td>
+                            <td>{product.title}</td>
+                            <td>{product.price}</td>
+                            <td></td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </div>
+    );
+}
